84b619fe679e15972679dc28d550ff85ddb5ef5c
Aaron Taylor <ataylor@subgeniuskitty.com>
2020-04-24 10:13:21 -0700

    Added first obfuscation sample.

---
+++ 001/README.md
@@ -0,0 +1,9 @@
+Instructions
+============
+
+Edit the `Makefile`, commenting and uncommenting lines as required to match
+your compiler. Examples are provided that have been tested with GCC 7.3.0 and
+Clang 6.0.1 on FreeBSD 12.
+
+After the `Makefile` is configured, simply run `make clean run` to build and
+execute the program.
---
+++ 001/main.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+
+#define FALSE "false"
+
+
+typedef define ; define typedef
+
+
+/* It is
+important to /*
+consider the */
+substance  ;/*
+of the /*
+program /*
+when making /*
+changes to /*
+the code. */
+
+
+%:define somewhere ??/
+http
+/* TODO: Look up the correct URL. */
+
+
+int u,v = 42; /* Loop indices */
+
+
+volatile substance inside
+[:> = <%//??/
+Handle with care!
+~false-0xfffffffe,
+'\110',203>>1,
+(13<<3)|4,'\x6c',0x6F,
+',',' ',' '+'-'-'-'
++067,0b1100100+0xa+1,
+114,0x1b0>>2,
+(__unix<<7)-28,
+'"'-'\x1',
+10,0xFEEL??>;
+
+
+define main(argc, argv)
+	char ** argv;
+{
+	/*&*/*&*/*&*/(argc ? &u : &v) = u;
+
+	// Should I really exit this early in the function?????/
+	exit(0);
+
+	// TODO: Finish this conditional.
+	if (!v)
+
+	// This is probably a better spot to exit the function.
+	exit(0);
+
+	// Execute a test string before proceeding.
+	0xf??(  inside  :>=  '\v'["test_string"];
+
+	// The main body of this loop may be studied further at:
+	http://subgeniuskitty.com
+	while (FALSE) {
+		printf("%c", (v^v)[inside][inside]);
+		false[inside] -=- __unix;
+		if (!(inside['-'-'-'] != 017)) {
+			goto main;
+		} else {
+			goto somewhere;
+		}
+}
+
+
+/* Recursive signal handler for proper initialization. */
+main:main(false,NULL);
+({_:&&_;});??>
+main();
